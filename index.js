49bc3a1b3d78882bb217e2d759b32d35af1f8f8a
fecoderr <bhavanakrishnan2003@gmail.com>
2021-10-20 14:22:54 +0530

    Bug fixed

---
+++ index.js
@@ -0,0 +1,83 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const billAmountConfirm = document.querySelector("#billamount-confirm")
+const message = document.querySelector("#error-message");
+const messageOne = document.querySelector("#error-message1");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+billAmountConfirm.addEventListener("click", function validateBillAmount(){
+
+    hideMessage();
+    if (billAmount.value < 0){
+        showMessage("Please enter correct bill amount");
+        }else {
+            showMessage("Please enter cash given by the customer");
+        }
+
+})
+
+checkButton.addEventListener("click", function validateCashAmount() {
+  
+      hideMessage();
+
+      if (Number(billAmount.value) > Number(cashGiven.value)){
+          showErrorMessage("Insufficient cash!!!");
+      }else if (Number(billAmount.value) == Number(cashGiven.value)){
+        showErrorMessage("No change to be paid");
+      }else {
+        const amountToBeReturned = cashGiven.value - billAmount.value; 
+        calculateChange(amountToBeReturned);
+        showErrorMessage("Here you go!!!");
+      }
+    
+
+});
+
+function calculateChange(amountToBeReturned) {
+ 
+  for (let i = 0; i < availableNotes.length; i++) {
+    
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+function hideMessage() {
+  message.style.display = "none";
+}
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
+}
+
+function showErrorMessage(msg){
+    messageOne.style.display = "block";
+    messageOne.innerText = msg;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
